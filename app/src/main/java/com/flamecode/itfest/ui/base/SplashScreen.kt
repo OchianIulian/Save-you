65152f4d35cafb720a75c99e93c19566b5fe4866
RaVortex00 <rarespopa007@gmail.com>
2020-11-14 12:55:00 +0200

    Modified SplashScreen.kt

--- app/src/main/java/com/flamecode/itfest/ui/base/SplashScreen.kt
+++ app/src/main/java/com/flamecode/itfest/ui/base/SplashScreen.kt
@@ -1,7 +1,5 @@
 package com.flamecode.itfest.ui.base
 
-import android.animation.Animator
-import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,31 +11,47 @@ import android.view.animation.AnimationUtils.loadAnimation
 import android.widget.ImageView
 import android.widget.TextView
 import com.flamecode.itfest.R
-import java.util.*
+import com.flamecode.itfest.manager.FragmentManager
 
 class SplashScreen : Fragment() {
 
     lateinit var logo : ImageView
     lateinit var appName: TextView
-    lateinit var animation:Animation
+    lateinit var animation : Animation
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_spalsh_screeen, container, false)
 
-        getdata(view)
+        getData(view)
         startAnimation()
         return view
     }
 
     private fun startAnimation() {
+
         logo.startAnimation(animation)
         val alphaAnimation = AlphaAnimation(0.0f, 1.0f)
         alphaAnimation.duration = 2000
         appName.startAnimation(alphaAnimation)
+
+        animation.setAnimationListener(object : Animation.AnimationListener {
+
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+
+                val fragmentManager = FragmentManager(this@SplashScreen.fragmentManager!!)
+                fragmentManager.replaceFragment(StatisticsFragment())
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+
+        })
     }
 
-    private fun getdata(view: View) {
+    private fun getData(view: View) {
+
         logo = view.findViewById(R.id.logo)
         appName = view.findViewById(R.id.app_name)
         animation = loadAnimation(context, R.anim.pulse)
