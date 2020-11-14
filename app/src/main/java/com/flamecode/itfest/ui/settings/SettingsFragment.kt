a96ad8e38a33bd5a0a93f17804b32ae8a7ebfe12
Okiul <ochian_a@yahoo.ro>
2020-11-14 18:35:26 +0200

    Created motor to SettingsFragment

--- app/src/main/java/com/flamecode/itfest/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/flamecode/itfest/ui/settings/SettingsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import android.widget.Switch
 import androidx.transition.TransitionInflater
 import com.flamecode.itfest.R
 
@@ -17,11 +19,41 @@ class SettingsFragment : Fragment() {
         exitTransition = inflater.inflateTransition(R.transition.slide_right)
     }
 
+    private lateinit var switch_online : Switch
+    private lateinit var switch_dark: Switch
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+         val view =inflater.inflate(R.layout.fragment_settings, container, false)
+        getData(view)
+
+        switch_online.setOnClickListener{
+            if(switch_online.isChecked){
+                switch_online.text = "Online"
+                //TODO: se trece la modul online
+            } else {
+                switch_online.text = "Offline"
+                //TODO: se trece la modul offline
+            }
+        }
+
+        switch_dark.setOnClickListener{
+            if(switch_dark.isChecked){
+                switch_dark.text = "Dark mode"
+                //TODO: se trece la modul dark
+            } else {
+                switch_dark.text = "LightMode"
+                //TODO: se trece la modul light
+            }
+        }
+        return view
+    }
+
+    private fun getData(view: View) {
+        switch_online = view.findViewById(R.id.switch_online)
+        switch_dark = view.findViewById(R.id.switch_dark)
     }
 }
