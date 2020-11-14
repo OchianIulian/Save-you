67404e447e7fa823617d2e5eacd2b22ea2715cdd
RaVortex00 <rarespopa007@gmail.com>
2020-11-14 18:43:13 +0200

    Add Location

--- app/src/main/java/com/flamecode/itfest/ui/base/StatisticsFragment.kt
+++ app/src/main/java/com/flamecode/itfest/ui/base/StatisticsFragment.kt
@@ -1,18 +1,22 @@
 package com.flamecode.itfest.ui.base
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.transition.TransitionInflater
 import com.flamecode.itfest.R
 import com.flamecode.itfest.manager.FragmentManager
 import com.flamecode.itfest.model.CountryData
 import com.flamecode.itfest.model.GlobalData
-import com.flamecode.itfest.network.AnalyticsApi
 import com.flamecode.itfest.ui.settings.SettingsFragment
 import com.flamecode.itfest.utils.FormatNumber
 import com.flamecode.itfest.utils.KTD
@@ -20,8 +24,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
-import org.w3c.dom.Text
 import java.net.URL
+
 
 /**
  * This fragment will have a role around the home, being the first fragment that
@@ -32,19 +36,19 @@ import java.net.URL
  */
 class StatisticsFragment : Fragment() {
 
-    lateinit var settingsImageView : ImageView
+    lateinit var settingsImageView: ImageView
     lateinit var fragmentManager: FragmentManager
-    lateinit var globalAnalytics : GlobalData
+    lateinit var globalAnalytics: GlobalData
     lateinit var countryData: CountryData
 
-    lateinit var casesGlobalTextView : TextView
-    lateinit var deathGlobalTextView : TextView
-    lateinit var recoveredGlobalTextView : TextView
+    lateinit var casesGlobalTextView: TextView
+    lateinit var deathGlobalTextView: TextView
+    lateinit var recoveredGlobalTextView: TextView
 
-    lateinit var activeGlobalTextView : TextView
-    lateinit var criticGlobalTextView : TextView
-    lateinit var casesPerMillionTextView : TextView
-    lateinit var deathPerMillionTextView : TextView
+    lateinit var activeGlobalTextView: TextView
+    lateinit var criticGlobalTextView: TextView
+    lateinit var casesPerMillionTextView: TextView
+    lateinit var deathPerMillionTextView: TextView
 
 
     companion object {
@@ -66,9 +70,9 @@ class StatisticsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val view =  inflater.inflate(R.layout.fragment_statistics, container, false)
+        val view = inflater.inflate(R.layout.fragment_statistics, container, false)
 
-        GlobalScope.launch( context = Dispatchers.IO) {
+        GlobalScope.launch(context = Dispatchers.IO) {
 
             val response = URL("$baseUrl/all").readText()
             globalAnalytics = KTD().deserializeForGlobalData(response)
@@ -76,7 +80,7 @@ class StatisticsFragment : Fragment() {
             val response2 = URL("$baseUrl/countries/World").readText()
             countryData = KTD().deserializeForCountryData(response2)
 
-            GlobalScope.launch( context = Dispatchers.Main) {
+            GlobalScope.launch(context = Dispatchers.Main) {
 
                 updateFirstMaterialCard(globalAnalytics)
                 updateSecondMaterialCard(countryData)
@@ -90,7 +94,59 @@ class StatisticsFragment : Fragment() {
         return view
     }
 
-    private fun initViews(view : View) {
+    override fun onResume() {
+        super.onResume()
+
+
+        if (ContextCompat.checkSelfPermission(
+                context!!,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    activity!!,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                ActivityCompat.requestPermissions(
+                    activity!!,
+                    Array(1) { Manifest.permission.ACCESS_FINE_LOCATION }, 1
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    activity!!,
+                    Array(1) { Manifest.permission.ACCESS_FINE_LOCATION }, 1
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] === PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(
+                            context!!,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+
+    private fun initViews(view: View) {
 
         settingsImageView = view.findViewById(R.id.settings)
         settingsImageView.setOnClickListener {
@@ -109,11 +165,11 @@ class StatisticsFragment : Fragment() {
 
     private fun updateSecondMaterialCard(countryData: CountryData) {
 
-        MainScope().launch(context = Dispatchers.Main){
+        MainScope().launch(context = Dispatchers.Main) {
 
             val format = FormatNumber()
             activeGlobalTextView.text = format.format(countryData.active.toString())
-            criticGlobalTextView.text =  format.format(countryData.critical.toString())
+            criticGlobalTextView.text = format.format(countryData.critical.toString())
             casesPerMillionTextView.text = format.format(countryData.casesPerOneMillion.toString())
             deathPerMillionTextView.text = format.format(countryData.deathsPerOneMillion.toString())
         }
@@ -121,11 +177,11 @@ class StatisticsFragment : Fragment() {
 
     private fun updateFirstMaterialCard(globalData: GlobalData) {
 
-        MainScope().launch(context = Dispatchers.Main){
+        MainScope().launch(context = Dispatchers.Main) {
 
             val format = FormatNumber()
             casesGlobalTextView.text = format.format(globalData.cases.toString())
-            deathGlobalTextView.text =  format.format(globalData.deaths.toString())
+            deathGlobalTextView.text = format.format(globalData.deaths.toString())
             recoveredGlobalTextView.text = format.format(globalData.recovered.toString())
         }
     }
