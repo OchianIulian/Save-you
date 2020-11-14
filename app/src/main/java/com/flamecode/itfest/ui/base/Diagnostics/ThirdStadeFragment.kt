187de8e73c64eabc3f2b8692b4c51e2d73f734d4
Okiul <ochian_a@yahoo.ro>
2020-11-14 21:52:06 +0200

    Pause Stop timer

--- app/src/main/java/com/flamecode/itfest/ui/base/Diagnostics/ThirdStadeFragment.kt
+++ app/src/main/java/com/flamecode/itfest/ui/base/Diagnostics/ThirdStadeFragment.kt
@@ -6,22 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.flamecode.itfest.R
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.NonCancellable.isCancelled
-import java.time.Duration
-import java.util.*
 import java.util.concurrent.TimeUnit
-import kotlin.concurrent.timer
 
 class ThirdStadeFragment : Fragment() {
 
     private lateinit var countDownTimer: CountDownTimer
     private var milisDays:Long = 1000*60*1440*14
     private var sustainingTime: Int = 15
+    private lateinit var button_stop:ImageView
 
     private lateinit var duration: TextView
+    lateinit var timeRemaining : String
 
 
     @InternalCoroutinesApi
@@ -32,9 +34,21 @@ class ThirdStadeFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_third_stade, container, false)
         duration = view.findViewById(R.id.duration_quarantine)
+        button_stop = view.findViewById(R.id.stop)
+
+
+
         timer(milisDays, 1000).start()
-
-
+        var paused  = 1
+        button_stop.setOnClickListener {
+            if(paused == 1){
+                Toast.makeText(context, "Paused", Toast.LENGTH_SHORT).show()
+                paused = 0
+            } else{
+                Toast.makeText(context, "You still have" + timeRemaining + "days", Toast.LENGTH_SHORT).show()
+                paused = 0
+            }
+        }
         return view
     }
 
@@ -42,7 +56,7 @@ class ThirdStadeFragment : Fragment() {
     private fun timer(millisInFuture:Long, countDownInterval:Long):CountDownTimer{
         return object: CountDownTimer(millisInFuture,countDownInterval){
             override fun onTick(millisUntilFinished: Long){
-                val timeRemaining = timeString(millisUntilFinished)
+                 timeRemaining = timeString(millisUntilFinished).toString()
                 if (isCancelled){
                     duration.text = "${duration.text}\nStopped.(Cancelled)"
                     cancel()
@@ -50,6 +64,14 @@ class ThirdStadeFragment : Fragment() {
                     duration.text = (timeRemaining+1).toString()
                     if((timeRemaining+1).toInt() < sustainingTime){
                         //sendNotification
+                        val message = "Don't warry, try to feel good and resist" + duration + "days"
+                        var builder = NotificationCompat.Builder(context)
+                            .setSmallIcon(R.drawable.logo)
+                            .setContentTitle("Save you")
+                            .setContentText(message)
+                            .setStyle(NotificationCompat.BigTextStyle()
+                                .bigText("Much longer text that cannot fit one line..."))
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                         sustainingTime = (timeRemaining+1).toInt()
                     }
                 }
@@ -57,14 +79,12 @@ class ThirdStadeFragment : Fragment() {
 
             override fun onFinish() {
                 duration.text = "Congrats, now you can "
-
-                //button_start.isEnabled = true
-                //button_stop.isEnabled = false
+                button_stop.isEnabled = false
             }
         }
     }
 
-    private fun timeString(millisUntilFinished:Long):Long{
+    private fun timeString(millisUntilFinished:Long): Long {
         var millisUntilFinished:Long = millisUntilFinished
         val days = TimeUnit.MILLISECONDS.toDays(millisUntilFinished)
         millisUntilFinished -= TimeUnit.DAYS.toMillis(days)
